8beab4e622d8fc426db9c3f9d73bc41c765f3c28
Jimmy Andersson <jimmyandersson89@gmail.com>
2019-05-16 09:51:42 +0100

    Fixed the slug handling so the slug is not unique anymore

--- sql/content/setup.sql
+++ sql/content/setup.sql
@@ -8,7 +8,8 @@ CREATE TABLE `content`
 (
     `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `path` CHAR(120) UNIQUE,
-    `slug` CHAR(120) UNIQUE,
+    -- `slug` CHAR(120) UNIQUE,
+    `slug` CHAR(120),
 
     `title` VARCHAR(120),
     `data` TEXT,
